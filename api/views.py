5c106c66d695170e1420a0056bf07278a3251323
djumanov <diyorbekjumanov1120@gmail.com>
2023-08-04 08:45:06 +0500

    hello view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
 
-# Create your views here.
+
+@api_view(['GET'])
+def hello_world(request: Request) -> Response:
+    return Response({'message': 'Hello, world!'})
+
