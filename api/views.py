2cbc54d6c67842e8f9624eea709446d5bd8501ca
djumanov <diyorbekjumanov1120@gmail.com>
2023-08-04 09:03:13 +0500

    get query params

--- api/views.py
+++ api/views.py
@@ -3,7 +3,17 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def hello_world(request: Request) -> Response:
-    return Response({'message': 'Hello, world!'})
+    if request.method == 'GET':
+        # get query parameters from request
+        params = request.query_params
+
+        name = params.get('name')
+        if name:
+            data = {"message": f"hello, {name}"}
+        else:
+            data = {"message": "hello, world"}
+
+        return Response(data=data)
 
