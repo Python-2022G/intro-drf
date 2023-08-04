6c70bbd993523f91d64f884edc3a5a8c8ef0460d
djumanov <diyorbekjumanov1120@gmail.com>
2023-08-04 09:27:47 +0500

    models choices;

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS_CHOICES = (
+    ('todo', 'To Do'),
+    ('doing', 'Doing'),
+    ('done', 'Done')
+)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(default='')
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
